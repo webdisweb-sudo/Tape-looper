9ef0d62513ba38f793e1888d641e3678bd9e890b
webdisweb-sudo <webdisweb@gmail.com>
2026-06-16 22:30:56 +0200

    v12 app/src/main/java/com/tapelooper/app/MainActivity.java

--- app/src/main/java/com/tapelooper/app/MainActivity.java
+++ app/src/main/java/com/tapelooper/app/MainActivity.java
@@ -7,13 +7,19 @@ import android.Manifest;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.webkit.*;
+import android.util.Base64;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends Activity {
     WebView webView;
     ValueCallback<Uri[]> filePathCallback;
     static final int MIC_REQ = 1;
     static final int FILE_REQ = 2;
+    static final int STORAGE_REQ = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,9 @@ public class MainActivity extends Activity {
         s.setDomStorageEnabled(true);
         s.setAllowFileAccess(true);
         s.setAllowContentAccess(true);
+
+        // JavaScript interface for file saving
+        webView.addJavascriptInterface(new FileInterface(), "AndroidFS");
 
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
@@ -46,17 +55,64 @@ public class MainActivity extends Activity {
                 MainActivity.this.filePathCallback = filePathCallback;
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("audio/*");
-                startActivityForResult(Intent.createChooser(intent, "Выберите аудиофайл"), FILE_REQ);
+                intent.setType("*/*");
+                String[] mimeTypes = {"audio/*", "application/json"};
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+                startActivityForResult(Intent.createChooser(intent, "Выберите файл"), FILE_REQ);
                 return true;
             }
         });
 
+        // Request permissions
+        requestPermissions();
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    void requestPermissions() {
         if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, MIC_REQ);
         }
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_REQ);
+            }
+        }
+    }
 
-        webView.loadUrl("file:///android_asset/index.html");
+    class FileInterface {
+        // Save base64 data to file, returns path or error
+        @JavascriptInterface
+        public String saveFile(String base64Data, String subDir, String fileName) {
+            try {
+                // Base dir: Music/TapeLooper/subDir
+                File musicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+                File appDir = new File(musicDir, "TapeLooper");
+                File targetDir = new File(appDir, subDir);
+                targetDir.mkdirs();
+
+                File outFile = new File(targetDir, fileName);
+                byte[] data = Base64.decode(base64Data, Base64.DEFAULT);
+                FileOutputStream fos = new FileOutputStream(outFile);
+                fos.write(data);
+                fos.close();
+
+                // Notify MediaStore
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                intent.setData(Uri.fromFile(outFile));
+                sendBroadcast(intent);
+
+                return "OK:" + outFile.getAbsolutePath();
+            } catch (Exception e) {
+                return "ERR:" + e.getMessage();
+            }
+        }
+
+        @JavascriptInterface
+        public String getBasePath() {
+            File musicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+            return new File(musicDir, "TapeLooper").getAbsolutePath();
+        }
     }
 
     @Override
